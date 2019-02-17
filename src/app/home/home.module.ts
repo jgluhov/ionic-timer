564970cf9e684ea2ca364cfabc9dde60f74da2d8
Evgeny Glukhov <evglukhov@luxoft.com>
2019-02-17 11:27:33 +0300

    complete timer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HomePage } from './home.page';
 
 @NgModule({
@@ -11,12 +11,24 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
         component: HomePage
       }
-    ])
+    ]),
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: '#78C000',
+      innerStrokeColor: '#C7E596',
+      animationDuration: 300,
+      animation: false,
+      responsive: false,
+      renderOnClick: false
+    })
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { interval, animationFrameScheduler, BehaviorSubject, Subject } from 'rxjs';
+import { map, takeWhile, takeUntil } from 'rxjs/operators';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  timeForm = this.fb.group({
+    durationTime: ['00:01:30']
+  });
+  localTimerPercent$ = new BehaviorSubject(0);
+  localTimerTeardown$ = new Subject();
+  overallTimerTeardown$ = new Subject();
+  localTimerRunning = false;
+  overallTimerRunning = false;
+  initialElapsedTime = { h: '00', m: '00', s: '00' };
+  overallTimerElapsedTime$ = new BehaviorSubject(this.initialElapsedTime);
+  constructor(
+    private insomnia: Insomnia,
+    private fb: FormBuilder
+  ) {}
 
+  ngOnInit() {
+    this.timeForm.valueChanges.subscribe(this.startTimer);
+  }
+
+  startTimer() {
+    this.restoreLocalTimer();
+
+    interval(1000, animationFrameScheduler).pipe(
+      map(this.toPercent(this.totalSeconds)),
+      takeWhile(this.atTheEnd(100)),
+      takeUntil(this.localTimerTeardown$),
+    ).subscribe(
+      {
+        next: (percent: number) => this.localTimerPercent$.next(percent),
+        complete: () => this.localTimerRunning = false
+      },
+    );
+
+    if (!this.overallTimerRunning) {
+      this.startOverallTimer();
+    }
+
+    this.localTimerRunning = true;
+  }
+
+  startOverallTimer() {
+    const countdownDate = Date.now();
+
+    interval(1000).pipe(
+      map(() => {
+        const now = Date.now();
+        const distance = now - countdownDate;
+        this.overallTimerElapsedTime$.next({
+          h: Math.floor( (distance % (1000 * 60 * 60 * 24) ) / (1000 * 60 * 60) ).toString().padStart(2, '0'),
+          m: Math.floor( (distance % (1000 * 60 * 60) ) / (1000 * 60) ).toString().padStart(2, '0'),
+          s: Math.floor( (distance % (1000 * 60) ) / 1000 ).toString().padStart(2, '0')
+        });
+      }),
+      takeUntil(this.overallTimerTeardown$)
+    ).subscribe();
+
+    this.overallTimerRunning = true;
+    this.insomnia.keepAwake();
+  }
+
+  restoreLocalTimer() {
+    this.localTimerRunning = false;
+    this.localTimerTeardown$.next();
+    this.localTimerPercent$.next(0);
+  }
+
+  restoreOverallTimer() {
+    this.overallTimerRunning = false;
+    this.overallTimerTeardown$.next();
+    this.overallTimerElapsedTime$.next(this.initialElapsedTime);
+  }
+
+  get totalSeconds() {
+    const { durationTime } = this.timeForm.value;
+    const [ hours, minutes, seconds ] = durationTime.split(':').map(Number);
+
+    return seconds + (minutes * 60) + (hours * 60 * 60);
+  }
+
+  toPercent = (totalSeconds: number) => (progress: number) => ++progress / totalSeconds * 100;
+  atTheEnd = (end: number) => (percent: number) => percent <= end;
+
+  restoreTimers() {
+    this.restoreLocalTimer();
+    this.restoreOverallTimer();
+    this.insomnia.allowSleepAgain();
+  }
 }
